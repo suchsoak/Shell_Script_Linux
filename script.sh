a33758bd352f4fa4e7b44ecc01f8fe185d1322cb
~#M?x <89079595+suchsoak@users.noreply.github.com>
2023-07-22 00:59:39 -0300

    script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash/
 # [[ "$OSTYPE" == "linux-gnu"*
+# || [ "$(OSTYPE)" == "linux-gnu" ] || [ "GNU/Linux" ]
 
 echo
 echo -e "\e[31m-------------------------| Github: https://github.com/suchsoak | -------------------------\e[0m"
@@ -8,7 +9,17 @@ sleep 7 > /dev/null
 
 clear
 
-if [ "$(lsb_release -si)" == "Debian" ] || [ "$(lsb_release -si)" == "Ubuntu" ] || [ "$(OSTYPE)" == "linux-gnu" ] || [ "GNU/Linux" ]; then+
+echo
+echo -e "\e[31m-------------------------| lsb-release | -------------------------\e[0m"
+echo
+
+sudo pacman -S lsb-release --noconfirm
+
+sudo apt install lsb-release -y
+
+clear
+
+if [ "$(lsb_release -si)" == "Debian" ] || [ "$(lsb_release -si)" == "Ubuntu" ]; then
 
 echo
 echo -e "\e[31m-------------------------| PING | -------------------------\e[0m"
@@ -115,50 +126,80 @@ clear
 
  elif [ "$(lsb_release -si)" == "Arch" ]; then
     echo
-    echo "-------------------------UPDATE-------------------------"
+    echo -e "\e[31m-------------------------| PING | -------------------------\e[0m"
     echo
-    sudo pacman -Syu --noconfirm
+
+        if ping -c 3 www.google.com; then
+        echo
+        else
+            echo "Internet not connected"
+        fi
+
+    sleep 3 > /dev/null
+
     clear
+
     echo
-    echo "------------------------- | Pacotes |-------------------------"
+    echo -e "\e[35m-------------------------| UPDATE | -------------------------\e[0m"
+    echo
+
+    sudo pacman -Sy --noconfirm
+    clear
+
+    echo
+    echo -e "\e[90m------------------------- | curl,wget,git,vim,net-tools,neofetch |-------------------------\e[0m"
     echo
 
     sudo pacman -Sy curl wget git vim make net-tools --noconfirm
+
     clear
+    
     echo
-    echo "------------------------- | Python, ruby, C, nodejs... | -------------------------"
+    echo -e "\e[90m------------------------- | Python, ruby, C, nodejs... | -------------------------\e[0m"
     echo
     sleep 5 > /dev/null
 
     sudo pacman -Sy build-essential --noconfirm
-    sudo pacman -Sy ruby lua5.3 python3 python3-pip nodejs --noconfirm
+    sudo pacman -Sy ruby lua5.3 python3 python3-pip nodejs gcc g++ --noconfirm
 
     clear
 
     echo
-    echo "-------------------------Baixando Linguagens De Programação-------------------------"
+    echo -e "\e[34m------------------------- VISUAL CODE STUDIO -------------------------"
     echo
-    sudo pacman -S gcc g++ ruby lua python nodejs --noconfirm
-    clear
-    echo
-    echo "-------------------------VISUAL CODE STUDIO-------------------------"
-    echo
+
+    sleep 3 > /dev/null
+    
     sudo pacman -S code --noconfirm
+
     clear
+
     echo
-    echo "-------------------------BRAVE BROWSER-------------------------"
+    echo -e "\e[34m------------------------- | VISUAL_STUDIO_CODE | -------------------------\e[0m"
     echo
+    sleep 3 > /dev/null
+
+    clear
+
+    echo
+    echo -e "\e[33m------------------------- BRAVE BROWSER -------------------------"
+    echo
+
     sudo pacman -S git --noconfirm
     git clone https://aur.archlinux.org/yay.git
     cd yay
+
+    sleep 4 > /dev/null
+
     makepkg -si
     yay -S brave-bin
     yay -S brave-beta-bin
     yay -S brave-nightly-bin
-    echo
-    echo "-------------------------BRAVE_BROWSER-------------------------"
-    echo
-else
-    echo "Sistema operacional não compatível com este script."
-fi
 
+    echo
+    echo -e "\e[33m------------------------- | BRAVE_BROWSER | -------------------------\e[0m"
+    echo
+
+else
+    echo "Operating system not supported by this script."
+fi
