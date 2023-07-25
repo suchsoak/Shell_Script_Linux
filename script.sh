f83919440b3e0be304fdd25b1dd0d5cf10e81f59
~#M?x <89079595+suchsoak@users.noreply.github.com>
2023-07-25 00:44:36 -0300

    script.sh

--- script.sh
+++ script.sh
@@ -17,9 +17,11 @@ sudo pacman -S lsb-release --noconfirm
 
 sudo apt install lsb-release -y
 
+sudo dnf install redhat-lsb-core
+
 clear
 
-if [ "$(lsb_release -si)" == "Debian" ] || [ "$(lsb_release -si)" == "Ubuntu" ] || [ "$(lsb_release -si)" == "Linuxmint" ] || [ "$(lsb_release -si)" == "kali" ] || [ "$(lsb_release -a)" == "vera" ] if [ "$(lsb_release -si)" == "Debian" ] || [ "$(lsb_release -si)" == "Ubuntu" ] || [ "$(lsb_release -si)" == "Linuxmint" ] || [ "$(lsb_release -si)" == "kali" ] || [ "$(lsb_release -a)" == "vera" ] || [ "$(lsb_release -si)" == "Fedora" ]; then
+if [ "$(lsb_release -si)" == "Debian" ] || [ "$(lsb_release -si)" == "Ubuntu" ] || [ "$(lsb_release -si)" == "Linuxmint" ] || [ "$(lsb_release -si)" == "kali" ] || [ "$(lsb_release -a)" == "vera" ]; then
 
 echo
 echo -e "\e[31m-------------------------| PING | -------------------------\e[0m"
@@ -183,7 +185,7 @@ clear
     sudo pacman -Sy curl wget git vim make net-tools wireless_tools xdg-utils openssh neofetch inxi git smartmontools --noconfirm
     echo
     echo
-    clear
+
     neofetch
 
     sleep 6 > /dev/null
@@ -282,6 +284,135 @@ clear
     yay -S brave-beta-bin --noconfirm
     yay -S brave-nightly-bin --noconfirm
     yay -Sy --noconfirm
+
+else
+    echo "Operating system not supported by this script."
+fi
+
+elif [ "$(lsb_release -si)" == "Fedora" ]; then
+
+    echo
+    echo -e "\e[31m-------------------------| PING | -------------------------\e[0m"
+    echo
+    echo
+
+        if ping -c 3 www.google.com; then
+        echo
+        else
+            echo "Internet not connected"
+        fi
+
+    sleep 3 > /dev/null
+
+    clear
+
+    echo
+    echo -e "\e[35m-------------------------| UPDATE | -------------------------\e[0m"
+    echo
+
+    sudo dnf update -y
+
+    clear
+
+    echo
+    echo -e "\e[90m------------------------- | PACKAGES |-------------------------\e[0m"
+    echo
+
+    sudo dnf install curl wget git vim make net-tools wireless_tools xdg-utils openssh neofetch inxi git smartmontools -y
+    echo
+    echo
+
+    neofetch
+
+    sleep 6 > /dev/null
+
+    clear
+    
+    echo
+    echo -e "\e[90m------------------------- | Programming language | -------------------------\e[0m"
+    echo
+
+    sleep 5 > /dev/null
+
+    echo
+    echo -e "\e[34m------------------------- | Gcc G++ |-------------------------\e[0m"
+    echo  
+
+
+    sleep 4 > /dev/null
+
+    sudo dnf install gcc g++ -y
+
+    sleep 4 > /dev/null
+
+    clear
+
+    echo
+    echo -e "\e[32m------------------------- | Nodejs |-------------------------\e[0m"
+    echo
+
+    sudo dnf install nodejs -y
+
+    sleep 4 > /dev/null
+
+    clear
+
+    echo
+    echo -e "\e[35m------------------------- | Lua | -------------------------\e[0m"
+    echo
+
+    sleep 4 > /dev/null
+
+    sudo dnf install lua -y
+
+    sleep 4 > /dev/null
+
+    clear
+
+    echo
+    echo -e "\e[34m------------------------- | Python | -------------------------\e[0m"
+    echo
+
+    sleep 3 > /dev/null
+
+    clear
+
+    sudo dnf install python3 python-pip -y
+
+    sleep 4 > /dev/null
+
+    echo
+    echo -e "\e[31m-------------------------| Ruby | -------------------------\e[0m"
+    echo
+
+    sleep 3 > /dev/null
+
+    sudo dnf install ruby  -y
+
+    sudo dnf install update -y
+
+    clear
+
+    echo
+    echo -e "\e[34m------------------------- VISUAL CODE STUDIO -------------------------"
+    echo
+
+    sleep 3 > /dev/null
+    
+    sudo dnf install code -y
+
+    clear
+
+    echo
+    echo -e "\e[33m------------------------- BRAVE BROWSER -------------------------"
+    echo
+
+    sleep 3 > /dev/null
+    sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+    sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+    sudo dnf update
+    sudo dnf install brave-browser
+    
 
 else
     echo "Operating system not supported by this script."
